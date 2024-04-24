e1cf02986a38cf673c4a259d10b1ecf1f13d946b
Jhonatan <jhonatanhuaman76@gmail.com>
2024-04-24 17:50:55 -0500

    Creando clase categoriaDAO

--- farmacia/src/main/java/com/farmacia/dao/CategoriaDAO.java
+++ farmacia/src/main/java/com/farmacia/dao/CategoriaDAO.java
@@ -1,40 +1,220 @@
 package com.farmacia.dao;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import com.farmacia.entidad.Categoria;
 import com.farmacia.interfaces.ICategoriaDAO;
+import com.farmacia.util.MySqlConexion;
 
 public class CategoriaDAO implements ICategoriaDAO {
 
 	@Override
 	public int registrarCategoria(Categoria c) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Declarar una variable para el resultado
+		int r = -1;
+		
+		//Declarar objeto para la conexion
+		Connection cone = null;
+		
+		//Declarar objeto para manipular procedimiento almacenado
+		CallableStatement cstm = null;
+		try {
+			cone = MySqlConexion.miConexion();
+			
+			//Preparar el callableStatement
+			cstm = cone.prepareCall("{call SP_REGISTRAR_CATEGORIA(?)}");
+			
+			//Enviar los datos a cstm obtenidos por la memoria ram
+			cstm.setString(1, c.getNom_cate());
+			
+			//Ejecutamos el callablestatement
+			r=cstm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(cone!=null) cone.close();
+				if(cstm!=null) cstm.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return r;
 	}
 
 	@Override
 	public int modificarCategoria(Categoria c) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Declarar una variable para el resultado
+		int r = -1;
+		
+		//Declarar objeto para la conexion
+		Connection cone = null;
+		
+		//Declarar objeto para manipular procedimiento almacenado
+		CallableStatement cstm = null;
+		try {
+			cone = MySqlConexion.miConexion();
+			
+			//Preparar el callableStatement
+			cstm = cone.prepareCall("{call SP_MODIFICAR_CATEGORIA(?,?)}");
+			
+			//Enviar los datos a cstm obtenidos por la memoria ram
+			cstm.setInt(1, c.getNum_cate());
+			cstm.setString(2, c.getNom_cate());
+			
+			//Ejecutamos el callablestatement
+			r=cstm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(cone!=null) cone.close();
+				if(cstm!=null) cstm.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return r;
 	}
 
 	@Override
 	public int eliminarCategoria(int num_cate) {
-		// TODO Auto-generated method stub
-		return 0;
+		//Declarar una variable para el resultado
+		int r = -1;
+		
+		//Declarar objeto para la conexion
+		Connection cone = null;
+		
+		//Declarar objeto para manipular procedimiento almacenado
+		CallableStatement cstm = null;
+		try {
+			cone = MySqlConexion.miConexion();
+			
+			//Preparar el callableStatement
+			cstm = cone.prepareCall("{call SP_ELIMINAR_CATEGORIA(?)}");
+			
+			//Enviar los datos a cstm obtenidos por la memoria ram
+			cstm.setInt(1, num_cate);
+			
+			//Ejecutamos el callablestatement
+			r=cstm.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(cone!=null) cone.close();
+				if(cstm!=null) cstm.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return r;
 	}
 
 	@Override
 	public Categoria buscarCategoria(int num_cate) {
-		// TODO Auto-generated method stub
-		return null;
+		//Declaramos objeto cliente
+		Categoria c = new Categoria();
+		
+		//Declarar objeto para la conexion
+		Connection cone = null;
+		
+		//Declarar objeto para manipular procedimiento almacenado
+		CallableStatement cstm = null;
+		
+		//Declarar objeto ResultSet que tiene el resultado del SELECT, para hacer listado
+		ResultSet rs = null;
+		try {
+			cone = MySqlConexion.miConexion();
+			
+			//Preparar el callableStatement
+			cstm = cone.prepareCall("{call SP_BUSCAR_CATEGORIA(?)}");
+			
+			//Enviar los datos a cstm obtenidos por la memoria ram
+			cstm.setInt(1, num_cate);
+			
+			//Ejecutamos el callablestatement
+			rs=cstm.executeQuery();
+			
+			while(rs.next()) {
+				//Llenamos el objeto cliente con los datos
+				c.setNum_cate(rs.getInt(1));
+				c.setNom_cate(rs.getString(2));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(cone!=null) cone.close();
+				if(cstm!=null) cstm.close();
+				if(rs!=null) rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		return c;
 	}
 
 	@Override
 	public ArrayList<Categoria> listadoCategoria() {
-		// TODO Auto-generated method stub
-		return null;
+		//Declaramos la lista de los clientes
+		ArrayList<Categoria> data = new ArrayList<Categoria>();
+		
+		//Declarar objeto para la conexion
+		Connection cone = null;
+		
+		//Declarar objeto para manipular procedimiento almacenado
+		CallableStatement cstm = null;
+		
+		//Declarar objeto ResultSet que tiene el resultado del SELECT, para hacer listado
+		ResultSet rs = null;
+		
+		try {
+			cone = MySqlConexion.miConexion();
+			
+			//Preparar el callableStatement
+			cstm = cone.prepareCall("{call SP_LISTAR_CATEGORIA()}");
+			
+			//Ejecutamos el callablestatement, enviar lo que tiene cstm a rs
+			rs=cstm.executeQuery();
+			
+			//Llenamos la lista con los clientes de la base de datos
+			while(rs.next()) {
+				Categoria c = new Categoria();
+				c.setNum_cate(rs.getInt(1));
+				c.setNom_cate(rs.getString(2));
+				
+				data.add(c);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(cone!=null) cone.close();
+				if(cstm!=null) cstm.close();
+				if(rs!=null) rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return data;
 	}
 
 }
