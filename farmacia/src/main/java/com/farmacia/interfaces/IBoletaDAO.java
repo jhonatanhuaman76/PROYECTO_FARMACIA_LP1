c8af58accfc831349f6bf8750dacc57e3d2394f2
unknown <Gabriel>
2024-04-26 17:14:02 -0500

    Creando DAO Boleta y Factura

--- farmacia/src/main/java/com/farmacia/interfaces/IBoletaDAO.java
+++ farmacia/src/main/java/com/farmacia/interfaces/IBoletaDAO.java
@@ -6,6 +6,8 @@ import com.farmacia.entidad.Boleta;
 
 public interface IBoletaDAO {
 int registrarBoleta(Boleta b);
+int modificarBoleta(Boleta b);
+int eliminarBoleta(int cod_boleta);
 Boleta buscarBoleta(int cod_boleta);
 ArrayList<Boleta> listadoBoleta();
 }
